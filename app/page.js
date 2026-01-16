198f07c93307d4c800f0b9f0e96d4fb52c20e78d
Nico <72673830+NicolasHrt@users.noreply.github.com>
2026-01-16 21:28:57 +0100

    feat: change model

--- app/page.js
+++ app/page.js
@@ -54,7 +54,7 @@ export default function Home() {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
-                    model: 'gpt-4o',
+                    model: "gpt-5.2",
                     messages: newMessages,
                     stream: true
                 })
